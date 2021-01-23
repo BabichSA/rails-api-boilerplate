7c50a5c419f74d830d0f2a0a58b2d975243f9121
Sergey Babich <babichsa.ua@gmail.com>
2021-01-23 17:39:05 +0300

    knock

--- requests/GetToken.bash
+++
@@ -1,8 +0,0 @@
-curl --location --request POST 'http://localhost:3000/auth/signin' \
---header 'Content-Type: application/json' \
---data-raw '{
- "auth": {
-    "email": "admin@admin.com",
-    "password": "P@ssw0rd"
-  }
-}'
